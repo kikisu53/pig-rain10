32c431d804d2db3ff384d5ce15bad56187232abe
TingRu.Chen <so910327@gmail.com>
2017-07-19 15:35:15 +0800

    second commit

--- routes/list.js
+++ routes/list.js
@@ -2,35 +2,36 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-/*load jsonfile to form*/
-router.all('*', function(req, res, next){
-  fs.readFile('data/pig-city.json', function(err, data){
-    if(err) console.error(err);
-    res.locals.city = JSON.parse(data);
-    next();
-  });
-  fs.readFile('data/pig-county.json', function(err, data){
-    if(err) console.error(err);
-    res.locals.county = JSON.parse(data);
-    next();
-  });
-
-
-});
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var city = Object.keys(res.locals.city).map(function(e) {
-    return [e.toString(), res.locals.city[e]];
+  var city = JSON.parse(fs.readFileSync('data/pig-city.json', 'utf8'));
+  city = Object.keys(city).map((e) =>
+    [e.toString(), city[e]]
+  );
+  var county = JSON.parse(fs.readFileSync('data/pig-county.json', 'utf8'));
+  county = Object.keys(county).map((e) =>{
+    var counties = Object.keys(county[e]).map((i) =>
+      [i.toString(), county[e][i]]
+    );
+    return [e.toString(), counties];
   });
-  //console.log(res.locals.county)
+  var stop = JSON.parse(fs.readFileSync('data/pig-stop.json', 'utf8'));
+  stop = Object.keys(stop).map((e) =>
+    [e.toString(), stop[e]]
+  );
+  //console.log(city);
+  //console.log(county);
+  //console.log(stop);
   res.render('list', {
-    city
+    city,
+    county,
+    stop
   });
   //userEmail
   //res.send('respond with a resource');
 });
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-});
+});*/
 
 module.exports = router;
