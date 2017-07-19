179e175bd73b865f3c609a5bc888be1094ea41a8
aldy120 <aldy120@yahoo.com.tw>
2017-07-19 15:32:34 +0800

    update: data reading

---
+++ public/data/pig-timespan.js
@@ -0,0 +1,6 @@
+const pigTimespan = ['10分鐘', '1小時', '3小時', '6小時', '12小時', '24小時', '本日', '前一日', '前二日']
+// 0,       1        2        3      4         5        6       7        8
+
+if (typeof module != "undefined" && module.exports) {
+  module.exports = pigTimespan;
+}
