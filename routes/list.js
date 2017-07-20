8c16a10551e5432d833136f35e43708cd1f6e904
aldy120 <aldy120@yahoo.com.tw>
2017-07-20 22:09:39 +0800

    post document to pig-notification

--- routes/list.js
+++ routes/list.js
@@ -1,41 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var AWS = require('aws-sdk');
-AWS.config.update({region: 'us-west-2'});
+AWS.config.update({ region: 'us-west-2' });
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  res.render('list');
-=======
->>>>>>> 6e0972f2b0cda0309afdd08df8342c29ce1dcc57
+router.get('/', function (req, res, next) {
   //userEmail
   //res.send('respond with a resource');
   res.render('list');
 });
-router.post('/', function(req, res, next) {
-  //res.send('respond with a resource');
-});
-router.post('/', function(req, res, next) {
-  var table = 'pig-notification';
-
-  var name = req.body.name;
-  var area = req.body.area;
+router.post('/', function (req, res, next) {
+  var areaId = req.body.stop;
+  var user = req.session.user || 'example@test.case';
   var timespan = req.body.timespan;
   var threshold = req.body.threshold;
-
+  var TableName = 'pig-notification';
   var params = {
-    TableName: table,
+    TableName,
     Item: {
-      name,
-      "area-id": area,
-      "timespan-id": timespan,
-      threshold
+      'area-id': areaId,
+      'user': user,
+      'timespan': timespan,
+      'threshold': threshold
     }
-  };
+  }
   console.log('Adding a new item...')
-  docClient.put(params, function(err, data) {
+  docClient.put(params, function (err, data) {
     if (err) {
       console.error('Unable to add item. Error JSON: ', JSON.stringify(err, null, 2));
       return;
@@ -43,7 +34,7 @@ router.post('/', function(req, res, next) {
     console.log("Added item: ", JSON.stringify(data, null, 2));
   })
   res.json(req.body);
-})
+});
 
 
 module.exports = router;
