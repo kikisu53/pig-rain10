1933793385ab80f96f0c2c0a14d979c37cbb447a
aldy120 <aldy120@yahoo.com.tw>
2017-07-22 22:17:09 +0800

    add the full addr for notifications

--- routes/list.js
+++ routes/list.js
@@ -35,7 +35,7 @@ router.get('/', function (req, res, next) {
     var items = data.Items.map(item => {
       var addr = {
         id: item['area-id'],
-        value: pigArea[item['area-id']].addr
+        value: pigArea[item['area-id']].city + pigArea[item['area-id']].addr
       };
       var threshold = item.threshold + '公釐';
       var timespan = pigTimespan[item.timespan];
