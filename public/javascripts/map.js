9422d4e8c0bf9360d56be810c065a2aa3d59d3db
aldy120 <aldy120@yahoo.com.tw>
2017-07-27 18:25:09 +0800

    select tag trigger maps

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,4 +1,4 @@
-function test(ss){
+function test(ss) {
     alert(ss)
 }
 
@@ -20,33 +20,68 @@ function test(ss){
 // stop.addEventListener('change', ()=>{
 //   initMap();
 // });
-
+function showStationById(id) {
+    console.log(id);
+    infoWindow.close();
+    var marker = getMarkerById(id);
+    var pos = {
+        lat: marker.position.lat(),
+        lng: marker.position.lng()
+    }
+    console.log(pos);
+    map.setZoom(15);
+    map.setCenter(pos);
+    console.log(map.getCenter())
+    setTimeout(() => google.maps.event.trigger(marker, 'click'), 000);
+    
+}
+function getMarkerById(id) {
+    return markers.filter(marker => marker.id === id)[0];
+}
+var map;
+var markers;
+var infoWindow;
 function initMap() {
     var stopId = stop.value;
 
     //Create google map
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
-        center: {lat: 25, lng: 121}
+        center: { lat: 25, lng: 121 }
     });
-
+    infoWindow = new google.maps.InfoWindow();
     // Add some markers to the map.
-    var markers = [];
-    function mark(i, location){
+    markers = [];
+    function mark(i, location) {
         return new google.maps.Marker({
-            position: {lng: parseFloat(location.lon), lat: parseFloat(location.lat)},
+            position: { lng: parseFloat(location.lon), lat: parseFloat(location.lat) },
             id: i
         })
     }
-    for(let i in pigPos){
-        markers.push(mark(i,pigPos[i]))
+    for (let i in pigPos) {
+        markers.push(mark(i, pigPos[i]));
     }
     // When markers onclick
-    markers.map( v => v.addListener('click', () => test(v.id)));
+    markers.map(v => v.addListener('click', () => addInfoWindows(v)));
 
     // Add a marker clusterer to manage the markers.
     var markerCluster = new MarkerClusterer(map, markers,
-        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
-        minimumClusterSize: 3
+        {
+            imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+            minimumClusterSize: 3
         });
+}
+var addInfoWindows = (v) => {
+    var id = v.id;
+    var contentString
+    if (!pigArea[id]) {
+        contentString = '尚無資料';
+    } else {
+        contentString = ['行政區: ' + pigArea[id].city,
+        '測站: ' + pigArea[id].stop,
+        '位址: ' + pigArea[id].addr
+        ].join('<br>');
+    }
+    infoWindow.setContent(contentString);
+    infoWindow.open(map, v);
 }
